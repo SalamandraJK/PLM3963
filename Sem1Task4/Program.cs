ca6738cbc44d79daa2034552f0eb97c1cc0ab60c
MikhailPL <muke.jry@gmail.com>
2023-02-12 21:07:27 +0300

    Solution Task4

--- Sem1Task4/Program.cs
+++ Sem1Task4/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿// Задача на ввод трех чисел и определение маклимального из них
+
+Console.Write("Введите число А - ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B - ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число C - ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+if(numA > numB)
+{
+    if(numA>numC)
+    {
+        Console.WriteLine("Большее число: " +numA);
+    }    
+
+    else 
+    {
+
+        Console.WriteLine("Большее число: " +numC);
+    }
+}
+    if(numB > numC)
+    {
+        Console.WriteLine("Большее число: " +numB);
+    }
+    else
+    {
+            Console.WriteLine("Большее число: " +numC);  
+        }
