3c6902e164ffe3b40b47ae8eaacef14654f35659
MikhailPL <muke.jry@gmail.com>
2023-02-12 21:16:48 +0300

    Solution Task8

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа на ввод числа N и вывода всех чисел
+// являющихся чётными от 1 до N
+
+
+Console.Write("Введите число N - ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 2; i<= numN; i = i +2)
+{
+
+Console.Write(i + " ");
+
+}
