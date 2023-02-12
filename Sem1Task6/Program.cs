2dee87c2bd5744a55d137e845aafe669ea826b71
MikhailPL <muke.jry@gmail.com>
2023-02-12 21:12:07 +0300

    Solution Task6

--- Sem1Task6/Program.cs
+++ Sem1Task6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача на ввод числа и определение,
+// является ли это число чётным
+
+Console.Write("Введите число - ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if(numA %2 == 0)
+{
+    Console.WriteLine("Число является чётным");
+}
+
+else
+{
+    Console.WriteLine("Число не является чётным");
+}
