fab4848329d5694dc0640f8db67655cb9a3525ec
MikhailPL <muke.jry@gmail.com>
2023-02-12 20:57:06 +0300

    Solution Task2

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача на ввод дву чисел и определения
+// наибольшего и наименьшего из них
+Console.Write("Введите число А - ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число В - ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if(numA > numB)
+{
+    Console.WriteLine("Числа A > B");
+}
+
+else
+{
+    Console.WriteLine("Число В > A");
+}
