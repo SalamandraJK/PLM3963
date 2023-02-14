1de650c439e27dddeac22ec47a3845014444e869
MikhailPL <muke.jry@gmail.com>
2023-02-14 21:31:16 +0300

    Sem2, Solution Task12 and Task14

---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача, принимающая на вход два числа и выводить, является ли второе число кратным первому. 
+// Если второе число некратно первому, то программа выводит остаток от деления.
+
+System.Random numSintezator = new System.Random();
+
+int rndNumber1 = numSintezator.Next(1,100);
+
+int rndNumber2 = numSintezator.Next(1,100);
+
+if(rndNumber1%rndNumber2 == 0)
+
+{
+    Console.WriteLine("Кратно");
+}
+
+else
+{
+    Console.WriteLine("Не кратно" );
+    Console.WriteLine(rndNumber1%rndNumber2);
+}
---
+++ Sem2Task14/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача, принимающая на вход число и проверяет,
+//  кратно ли оно одновременно 7 и 23.
+
+
+Console.Write("Введите число - ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool oper = (num%21 == 0) && (num%7 == 0);
+
+if (oper)
+{
+
+Console.WriteLine("Кратно");
+    
+}
+
+else
+{
+
+Console.WriteLine("НЕ Кратно");
+
+Console.WriteLine(num%23);
+
+Console.WriteLine(num%7);
+
+}
