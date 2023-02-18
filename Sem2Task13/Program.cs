703d97dc1660c6d1cfd347c0858188887d4c4956
MikhailPL <muke.jry@gmail.com>
2023-02-18 21:39:02 +0300

    Sem2 - Decision Task13Practical

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача, на вывод третьёй цифры заданного числа и
+// ли сообщение того, что третьей цифры нет.
+
+// Ввод числа
+Console.Write("Введите число: ");
+// Считываем переменную числа
+String num = Console.ReadLine()??"0";
+// Преобразовываем число в массив
+char[] array = num.ToCharArray();
+// Находим длинну массива
+int ArrayLen = array.Length;
+// Условие для проверки по нахождению третьего числа цифры
+if (ArrayLen > 2)
+{
+Console.WriteLine($"Длинна массива: {ArrayLen}");
+Console.WriteLine($"Третье число: {array[2]}");
+}
+else
+{
+Console.WriteLine("Введено число, в котором нет третьей цифры");
+Console.WriteLine($"Длинна массива: {ArrayLen}");
+}
