380e23e60e7cb0b617f3522cb06f12fda31369be
MikhailPL <muke.jry@gmail.com>
2023-02-18 11:32:44 +0300

    Sem2, editing Task12

--- Sem2Task12/Program.cs
+++ Sem2Task12/Program.cs
@@ -1,18 +1,19 @@
 ﻿// Задача, принимающая на вход два числа и выводить, является ли второе число кратным первому. 
 // Если второе число некратно первому, то программа выводит остаток от деления.
 
+// Получение рандомные числа
 System.Random numSintezator = new System.Random();
 
+// Выводим полученный результат
 int rndNumber1 = numSintezator.Next(1,100);
-
 int rndNumber2 = numSintezator.Next(1,100);
 
+// Цикл получения результата от деления двух чисел
+// Вывод результата
 if(rndNumber1%rndNumber2 == 0)
-
 {
     Console.WriteLine("Кратно");
 }
-
 else
 {
     Console.WriteLine("Не кратно" );
