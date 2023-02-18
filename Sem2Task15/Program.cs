884d60e96267545fd5fce761614bb508982806f7
MikhailPL <muke.jry@gmail.com>
2023-02-18 21:55:13 +0300

    Sem2 - Practical task15

--- Sem2Task15/Program.cs
+++ Sem2Task15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача, на приём цифры, обозначающую день недели, и проверка его, является ли этот день выходным.
+
+// Вводин число(день недели)
+Console.Write("Введите число - ");
+int nume = int.Parse(Console.ReadLine()??"0");
+// Условия для проверки
+if(nume < 8 && nume > 0)
+{
+    if(nume > 5)
+    Console.WriteLine("Выходной день");
+    
+    if(nume < 5)
+     Console.WriteLine("Будний день");
+}
+else
+{
+    Console.WriteLine("Введено неверное число");
+}
