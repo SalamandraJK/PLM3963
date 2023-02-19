9fecad68dd5d9bbaf708e817c4b08d952fd08e09
MikhailPL <muke.jry@gmail.com>
2023-02-19 18:07:57 +0300

    Sem2 - Practical task15, tsk17, task18

---
+++ Sem3Task17/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача, принимаем на вход координаты точки (Х, Y)
+// и показываем в какой плоскости находится точка
+
+// <тип> <имя метода> (<тип><название переменной>)
+// {
+//     return <тип>
+// }
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void PrintQuterTest(int x, int y)
+{
+    if(x > 0 && y > 0)Console.WriteLine("Точка находится в Первой четверти.");
+    if(x > 0 && y < 0)Console.WriteLine("Точка находится во Второй четверти.");
+    if(x < 0 && y < 0)Console.WriteLine("Точка находится в Третьей четверти.");
+    if(x < 0 && y > 0)Console.WriteLine("Точка находится в Четвертой четверти.");
+}
+
+int coordX = ReadData("Введите координату Х: ");
+int coordY = ReadData("Введите координату Y: ");
+PrintQuterTest(coordX, coordY);
---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача, показать возможный диапозон координат точек,
+// в заданной плоскости
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void PrintAnswer(int num)
+{
+    if(num < 5 && num >0) 
+    {
+        if(num == 1)Console.WriteLine("Диапозон: X > 0, Y>0");
+        if(num == 2)Console.WriteLine("Диапозон: X < 0, Y>0");
+        if(num == 3)Console.WriteLine("Диапозон: X < 0, Y<0");
+        if(num == 4)Console.WriteLine("Диапозон: X > 0, Y<0");
+    }
+    else{
+        Console.WriteLine("Введён неверный номер четверти");
+    }
+}
+
+int quarter = ReadData("Введите номер четверти:");
+PrintAnswer(quarter);
