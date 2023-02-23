44d1f4ca2297d7fe93de8b1edcc3056be4e28982
MikhailPL <muke.jry@gmail.com>
2023-02-23 13:34:53 +0300

    Sem3 - correction Task23

---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача №22
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+int ReadInput(string mess)
+{
+    Console.Write(mess);
+    return Convert.ToInt32(Console.ReadLine() ?? "0");
+}
+
+string LineBuilder(int Num, int Pow)
+{
+    string result = String.Empty;
+    for(int i = 1; i <= Num; i++)
+    {
+        result = result + Math.Pow(i, Pow)+"\t !";
+    }
+
+    return result;
+}
+
+int N = ReadInput("Введите число: ");
+
+Console.WriteLine(LineBuilder(N,1));
+Console.WriteLine(LineBuilder(N,3));
+
+
