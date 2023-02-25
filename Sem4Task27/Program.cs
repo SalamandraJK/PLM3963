606b1ec92cdf133a661af37f8c399e1c18be4dfc
MikhailPL <muke.jry@gmail.com>
2023-02-25 16:22:55 +0300

    Writ HW Task27

---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 27: 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// Добавляем метод принимающий данные от пользователя
+int ReadData(string line)
+{
+    // Вывод сообщения
+    Console.Write(line);
+    // Считываем данные от пользователя
+    int number = int.Parse(Console.ReadLine()??"0");
+    // Возвращаем полученное значение
+    return number;
+}
+
+// Вводим метод вывода получаемого результата
+void PrintData(string line)
+{
+    Console.WriteLine(line);
+}
+
+// Вводим метод посчета суммы цифр в веденном числе
+int SumNumber(int N)
+{
+    int res = 0;
+    while(N>0)
+    {
+        res = res + N%10;
+        N = N/10;
+    }
+return res;
+}
+
+// Присваеваем внасимое пользователем значение в введенную переменную numberA.
+int numN = ReadData("Введите число N: ");
+
+// Вводим переменную resSum и прередаём в нее значение, получаемое в методе SumNumber
+long resSum = SumNumber(numN);
+
+Console.WriteLine(" ");
+PrintData("Сумма цифр в числе N = " + resSum);
