bc8d1e73ee5d8889ec4b1e1e32fc52bb9e0255d5
MikhailPL <muke.jry@gmail.com>
2023-03-02 13:21:21 +0300

    Editing Task 34

--- Sem5Task34/Program.cs
+++ Sem5Task34/Program.cs
@@ -47,22 +47,6 @@ int EverNum(int[] arr)
     return (SumEven);
 }
 
-// Метод, для сортировки масси пузырьком
-// void BubbleSort(int[] arr)
-// {
-// for (int i = 0; i < arr.Length; i++)
-// {
-//     for (int j = i + 1; j < arr.Length; j++)
-//     {
-//         if (arr[i] > arr[j])
-//            {
-//             int temp = arr[i];
-//             arr[i] = arr[j];
-//             arr[j] = temp;
-//             }                   
-//     }            
-// }
-// }
 void BubbleSort(int[] arr)
 {
 int temp = 0;
