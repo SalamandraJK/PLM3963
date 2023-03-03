45dde52b8a08a4bbd1a51c895331e9ff9be8cc27
MikhailPL <muke.jry@gmail.com>
2023-03-04 00:46:42 +0300

    Editing Task 42

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+// Метод, для приема данных
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод, для печати принятых данных
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Метод конвектирующий входящее 10-ое число в 2-ое
+string DecToBin(int num)
+{
+    string res = String.Empty;
+    while ( num>0)
+    {
+        res = num%2 + res;
+        num = num / 2;
+    }
+    return res;
+}
+
+int Num = ReadData("Введите десятичное число: ");
+
+string NumII = DecToBin(Num);
+
+PrintData($"В двоичной системе: {NumII}");
