b71b889702714ed45e75b32f37ff7cc735d4934b
MikhailPL <muke.jry@gmail.com>
2023-03-04 16:09:06 +0300

    Editing Task 41

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает от Пользователя M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл Пользователь.
+
+// Метод, для приема данных
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод, для печати принятых данных
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int CountNum(int numM)
+{
+    int res = 0;
+    for(int i = 0; i<numM; i++)
+    {
+        if(ReadData("Введите число: ")>0)
+        {
+            res++;
+        }
+    }
+    return res;
+}
+
+int M = ReadData("Введите колличество чисел от 0 до М : ");
+int Size = CountNum(M);
+PrintData($"Колличество чисел > 0: {Size}");
