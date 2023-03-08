51b237d4f7f11b2c0621754cb87b83b84ef628a9
MikhailPL <muke.jry@gmail.com>
2023-03-08 15:03:44 +0300

    Editing Task50

---
+++ Sem8Task55/Program.cs
@@ -0,0 +1,94 @@
+﻿// Задача №55
+// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+// Метод, для приема данных
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Вводим метод, заполняющий массив (генерация)
+int[,] Gen2DArr( int min, int max, int RowCount, int ColCount)
+{
+    // Проверка входных данных массива
+    int buf;
+    if (min > max)
+    {
+        buf = min;
+        min = max;
+        max = buf;
+    }
+    // Генерация двумерного массива
+    int[,] binarray = new int[RowCount, ColCount];
+    System.Random numSintezator = new System.Random();
+    for (int i = 0; i < RowCount; i++)
+    {
+        for (int j = 0; j < ColCount; j++)
+        {
+        
+            binarray[i,j] = numSintezator.Next(min, max);
+            
+        }
+    }
+    return binarray;
+}
+
+// Вводин метод, который печатает одномерный массив
+void Print2DArr(int[,] arr)
+{
+    // Вводим массив содержащий различные цвета
+    ConsoleColor[] color = new ConsoleColor[]{ConsoleColor.Black, ConsoleColor.Blue, 
+                                            ConsoleColor.Cyan, ConsoleColor.DarkBlue, 
+                                            ConsoleColor.DarkCyan, ConsoleColor.DarkGray,
+                                            ConsoleColor.DarkGreen, ConsoleColor.DarkMagenta,
+                                            ConsoleColor.DarkRed, ConsoleColor.DarkYellow,
+                                            ConsoleColor.Gray, ConsoleColor.Green,  
+                                            ConsoleColor.Magenta, ConsoleColor.Red, 
+                                            ConsoleColor.White, ConsoleColor.Yellow};
+    // Console.Write("[");
+    for(int i=0; i<arr.GetLength(0); i++)
+    {
+        for(int j=0; j<arr.GetLength(1); j++)
+        {
+            Console.ForegroundColor = color[new Random().Next(2,2)];
+            Console.Write(arr[i,j] + "\t");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+void TransportMatrix(int[,] arr)
+{
+    int temp = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = i+1; j < arr.GetLength(1); j++)
+        {
+            temp = arr[i, j];
+            arr[i, j] = arr[j,i];
+            arr[j,i] = temp;
+        }
+    }
+}
+
+// Ввод параметров массива(строки с стлобцы)
+int m = ReadData("Введите колличество строк: ");
+int n = ReadData("Введите колличество столбцов: ");
+// Генерация и печать массива
+int[,] arr2D = Gen2DArr(10, 99, m, n);
+Print2DArr(arr2D);
+Console.WriteLine(" ");
+
+if(arr2D.GetLength(0) == arr2D.GetLength(1))
+{
+// Печать транспонированного массива
+TransportMatrix(arr2D);
+Print2DArr(arr2D);
+}
+else
+{
+    Console.WriteLine("Массив не может быть транспонирован");
+}
---
+++ Sem8Task61/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача №61
+// Вывести первые N строк треугольника Паскаля. 
+// Сделать вывод в виде равнобедренного треугольника
+
+// Метод, для приема данных
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//n!/k!*(n-k)! - формула для вычисления конкретного элемента треугольника
+
+long Factroreal(int n)
+{
+    long res = 1;
+    for(int i=1; i<=n; i++)
+    {
+        res = res*i;
+    }
+    return res;
+}
+
+void PrintPascalTriangle(int nRow)
+{
+    for(int i=0; i<nRow; i++)
+    {
+        for(int k=0; k<(nRow)-i; k++)
+        {
+            Console.Write(" ");
+        }
+        for(int j=0; j<i; j++)
+        {
+            Console.Write(" ");
+            Console.Write(Factroreal(i)/(Factroreal(j)*Factroreal(i-j)));
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int countRow = ReadData("Введите колличество строк для треугольника Паскаля: ");
+PrintPascalTriangle(countRow);
