4cd2dcf28cfca4c70dfe26360dde6ed7664d175d
MikhailPL <muke.jry@gmail.com>
2023-03-12 15:51:44 +0300

    Editing Task60

---
+++ Sem8Task60/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 60.
+// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+
+// Метод, для приема данных
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод генерации Трехмерного массива
+int[,,] CreateMatrix (int row, int col, int dep, int min, int max) 
+{
+    int[,,] matrix = new int[row, col, dep];
+    Random rnd = new Random();   
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+                {
+                    matrix[i,j,k] = rnd.Next(min, max + 1);
+                }
+        }
+    }
+    return matrix;
+} 
+ 
+// Метод печати Трехмерного массива
+void Print3DMatrix(int[,,] matrix)
+{
+    for (int i = 0; i <  matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+            Console.Write("|");
+            Console.Write($"{matrix[i, j, k],1}({i},{j},{k})|"); 
+            Console.WriteLine();           
+            }
+        }
+        Console.WriteLine();        
+    }        
+}
+ 
+// Ввод параметров массива(строки с стлобцы)
+int length = ReadData("Введите значение длинны: ");
+int width = ReadData("Введите значение ширины: ");
+int height = ReadData("Введите значение высоты: ");
+int min = ReadData("Введите минимальное значение элемента: ");
+int max = ReadData("Введите максимальное значение элемента: ");
+// Генерация и печать массива
+int[,,] array3D = CreateMatrix(length, width, height, min, max);
+Print3DMatrix(array3D);
--- TestCode/Program.cs
+++ TestCode/Program.cs
@@ -86,70 +86,40 @@
 //                 Console.WriteLine();
 //             }
 // }
-using System;
- 
-namespace ConsoleApplication1
+
+int[,,] CreateMatrix (int row, int col, int dep, int min, int max) 
 {
-    class Program
+    int[,,] matrix = new int[row, col, dep];
+    Random rnd = new Random();   
+    for (int i = 0; i < matrix.GetLength(0); i++) 
     {
-        static void Main(string[] args)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.WriteLine("Введите размерность первой матрицы: ");
-            int[,] A = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
-            for (int i = 0; i < A.GetLength(0); i++)
-            {
-                for (int j = 0; j < A.GetLength(1); j++)
+            for (int k = 0; k < matrix.GetLength(2); k++)
                 {
-                    Console.Write("A[{0},{1}] = ", i, j);
-                    A[i, j] = Convert.ToInt32(Console.ReadLine());
+                    matrix[i,j,k] = rnd.Next(min, max + 1);
                 }
-            }
-            Console.WriteLine("Введите размерность второй матрицы: ");
-            int[,] B = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
-            for (int i = 0; i < B.GetLength(0); i++)
-            {
-                for (int j = 0; j < B.GetLength(1); j++)
-                {
-                    Console.Write("B[{0},{1}] = ", i, j);
-                    B[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
-            }
- 
-            Console.WriteLine("\nМатрица A:");
-            Print(A);
-            Console.WriteLine("\nМатрица B:");
-            Print(B);
-            Console.WriteLine("\nМатрица C = A * B:");
-            int[,] C = Multiplication(A, B);
-            Print(C);
- 
-        }
-        static int[,] Multiplication(int[,] a, int[,] b)
-        {
-            if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
-            int[,] r = new int[a.GetLength(0), b.GetLength(1)];
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < b.GetLength(1); j++)
-                {
-                    for (int k = 0; k < b.GetLength(0); k++)
-                    {
-                        r[i,j] += a[i,k] * b[k,j];
-                    }
-                }
-            }
-            return r;
-        }
-        static void Print(int[,] a)
-        {
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < a.GetLength(1); j++)
-                {
-                    Console.Write("{0} ", a[i, j]);
-                }
-                Console.WriteLine();
-            }
         }
     }
+    return matrix;
+} 
+ 
+void Print3DMatrix(int[,,] matrix)
+{
+    for (int i = 0; i <  matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+            Console.Write("|");
+            Console.Write($"{matrix[i, j, k],1}({i},{j},{k})|"); 
+            Console.WriteLine();           
+            }
+        }
+        Console.WriteLine();        
+    }        
 }
+ 
+int[,,] array3D = CreateMatrix(2, 2, 2, 10, 99);
+Print3DMatrix(array3D);
