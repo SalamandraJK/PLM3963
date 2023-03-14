263761406a9732b03365d6cd2609f9b677f2fc07
MikhailPL <muke.jry@gmail.com>
2023-03-14 22:34:07 +0300

    Editing Task63,65

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// 
+
+// Метод, для приема данных
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод вывода результата
+void PrintData(string res, int value)
+{
+    Console.WriteLine(res + value);
+}
---
+++ Sem9Task65/Program.cs
@@ -0,0 +1,37 @@
+﻿// 
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод вывода результата
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+
+string RecMiNi(int m, int n)
+{
+    string res = String.Empty;
+    if(m>=n)
+    {
+        res = res + n;
+    }
+    else
+    {
+        res = res + m + " " + RecMiNi(m+1,n);
+    }
+    return res;
+}
+
+int N = ReadData("Введите число N: ");
+int M = ReadData("Введите число M: ");
+
+string result = N<M?RecMiNi(N, M):RecMiNi(M,N);
+
+PrintData(result);
