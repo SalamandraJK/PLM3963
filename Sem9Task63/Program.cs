212f4c3a1f4986d81fcaf6ca7b77817046dba65c
MikhailPL <muke.jry@gmail.com>
2023-03-14 22:50:38 +0300

    Editing Task67

--- Sem9Task63/Program.cs
+++ Sem9Task63/Program.cs
@@ -1,53 +1,24 @@
 ﻿// Задача 63. Задайте значение N. 
 // Нужно вывести все натуральные числа от 1 до N. Выполнить с помощью рекурсии.
 
-// // Метод, для приема данных
-// int ReadData(string line)
-// {
-//     Console.Write(line);
-//     int number = int.Parse(Console.ReadLine() ?? "0");
-//     return number;
-// }
-// // Метод вывода результата
-// // void PrintData(string res, int value)
-// // {
-// //     Console.WriteLine(res + value);
-// // }
-
-
-// void LineGenRec(int num)
-// {
-//     Console.Write(num + " ");
-//     if(num == 1)
-//     {
-//     }
-//     else
-//     {
-//         LineGenRec(num - 1);
-//     }
-// }
-
-// int N = ReadData("Введите число N: ");
-// LineGenRec(N);
-
-// Чтение данных из консоли
+// Метод, для приема данных
 int ReadData(string line)
 {
     Console.Write(line);
     int number = int.Parse(Console.ReadLine() ?? "0");
     return number;
 }
+// Метод вывода результата
+// void PrintData(string res, int value)
+// {
+//     Console.WriteLine(res + value);
+// }
 
-// LineGenRec(5)
-// LineGenRec(4)
-// LineGenRec(3)
-// LineGenRec(2)
-// LineGenRec(1)
 
 void LineGenRec(int num)
-{   
+{
     Console.Write(num + " ");
-    if (num == 1)
+    if(num == 1)
     {
     }
     else
---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,30 @@
+﻿// 
+
+// Чтение данных из консоли
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод вывода результата
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+
+int RecSumDigit(int num)
+{
+    if(num == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return num%10+RecSumDigit(num/10);
+    }
+}
+
+int N = ReadData("Введите число N: ");
+PrintData($"Сумма: {RecSumDigit(N)}");
