108a2471a06e6f4f0f14845e5c0052d02edba40e
MikhailPL <muke.jry@gmail.com>
2023-03-15 20:20:30 +0300

    Editing Task69

--- Sem9Task69/Program.cs
+++ Sem9Task69/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+
+// Чтение данных из консоли
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод вывода результата
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+
+// double RecPow(double n, double m)
+// {
+//     if(n == 0 && m == 0)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return Math.Pow(n, m);
+//     }
+// }
+
+double RecPow(double n, double m)
+{
+    if(m<=1) return n;
+    
+    else return n*RecPow(n, m-1);
+}
+
+int N = ReadData("Введите число N: ");
+int M = ReadData("Введите число M: ");
+PrintData($"Возведение в сеперь M числа N = {RecPow(N, M)}");
