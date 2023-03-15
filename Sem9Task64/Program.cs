47b899e43d4be3cc12846bec8c428702236ff110
MikhailPL <muke.jry@gmail.com>
2023-03-15 20:48:07 +0300

    Editing Task66

--- Sem9Task64/Program.cs
+++ Sem9Task64/Program.cs
@@ -1,14 +1,26 @@
-﻿// 
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N. 
+// Выполнить с помощью рекурсии.
 
 // Метод, для приема данных
-int ReadData(string message)
+int ReadData(string line)
 {
-    Console.Write(message);
-    return int.Parse(Console.ReadLine() ?? "0");
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
 }
 
-// Метод вывода результата
-void PrintData(string res, int value)
+void LineGenRec(int num)
 {
-    Console.WriteLine(res + value);
+    Console.Write($"{ num }, ");
+    if(num == 1)
+    {
+    }
+    else
+    {
+        LineGenRec(num - 1);
+    }
 }
+
+int number = ReadData("Введите число N: ");
+LineGenRec(number);
---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте значения M и N. Напишите программу,  
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Метод, для приема данных
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+// Метод вывода результата
+void PrintData(int value)
+{
+    Console.WriteLine(value);
+}
+
+
+int RecSum(int M, int N)
+{
+    if(M >= N)
+    {
+        return N;
+    }
+    else
+    {
+        RecSum(M + 1, N);
+    }
+    return M + RecSum(M + 1, N);
+}
+
+int numN = ReadData("Введите число N: ");
+int numM = ReadData("Введите число M: ");
+PrintData(RecSum(numM, numN));
